80a314144b45582b5b3fafd6f99538ca5c83a6fa
imwyw <now_way@126.com>
2019-12-05 17:31:08 +0800

    开发环境

--- src/webpack-sample/webpack.config.js
+++ src/webpack-sample/webpack.config.js
@@ -1,14 +1,19 @@
 // Node内置的path模块，用于路径相关操作
 const path = require('path');
 // 默认将会在 output.path 的目录下创建一个 index.html 文件， 并在这个文件中插入一个 script 标签，标签的 src 为 output.filename。
-const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 // 用于清理dist文件夹插件
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
         app: './src/index.js',
         print: './src/print.js'
+    },
+    // 用于开发环境调试
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
     },
     plugins: [
         new CleanWebpackPlugin(),
@@ -19,6 +24,7 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         // __dirname 系统变量，当前根路径
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     }
 };
