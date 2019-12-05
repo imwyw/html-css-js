3e98b4d530f1392b3b20c4b9d0cb8b1bdbd336e5
imwyw <now_way@126.com>
2019-12-05 17:03:40 +0800

    管理输出

--- src/webpack-sample/webpack.config.js
+++ src/webpack-sample/webpack.config.js
@@ -1,9 +1,24 @@
+// Node内置的path模块，用于路径相关操作
 const path = require('path');
+// 默认将会在 output.path 的目录下创建一个 index.html 文件， 并在这个文件中插入一个 script 标签，标签的 src 为 output.filename。
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+// 用于清理dist文件夹插件
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
+        // __dirname 系统变量，当前根路径
         path: path.resolve(__dirname, 'dist')
     }
 };
