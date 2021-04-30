24c1747cb0a8f1137357c770e03af3b259715f1d
imwyw <now_way@126.com>
2021-04-30 16:36:31 +0800

    初始化项目； 父子组件；

---
+++ src/vue-demo/todolist/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nav-header></nav-header>
+    <nav-main></nav-main>
+    <nav-footer></nav-footer>
+  </div>
+</template>
+
+<script>
+import NavHeader from '@/components/header/NavHeader'
+import NavMain from '@/components/main/NavMain'
+import NavFooter from '@/components/footer/NavFooter'
+
+export default {
+  name: 'Home',
+  components: { NavHeader, NavMain, NavFooter },
+  setup() {}
+}
+NavFooter
+</script>
+
+<style lang="scss" scoped></style>
